6eba2e97849fbc20fd1556b7b0ec77ef613f9d8b
Lelick <lelick@list.ru>
2022-02-03 07:15:49 +0200

    added - header, slider, top products, popup catalog, modal-cart

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,124 @@
-$(finction({
+$(function () {
 
-}));
+  var $doc = $(document);
+  var $win = $(window);
+
+  $('.user-menu__style--search').on('click', function () {
+    $('.search').toggleClass('search--active');
+  });
+
+  $('.user-menu__style--cart, .cart-modal__close-button').on('click', function () {
+    $('.modal--cart').toggleClass('modal--active');
+    $('body').toggleClass('scroll-lock');
+  });
+
+  $('.catalog__btn').on('click', function () {
+    $('.catalog__btn').toggleClass('catalog__btn--active');
+    $('.catalog__list').toggleClass('catalog__list--active');
+  });
+
+  $(document).click(function (e) {
+    if (!$('.catalog__btn').is(e.target) && !$('.catalog__list').is(e.target) && $('.catalog__list').has(e.target).length === 0) {
+      $('.catalog__btn').removeClass('catalog__btn--active');
+      $('.catalog__list').removeClass('catalog__list--active');
+    };
+    if (!$('.user-menu__style--search').is(e.target) && !$('.search').is(e.target) && $('.search').has(e.target).length === 0) {
+      $('.search').removeClass('search--active');
+    };
+    if (!$('.modal__close-button').is(e.target) && !$('.user-menu__style--cart').is(e.target) && !$('.modal__cart').is(e.target) && $('.modal__cart').has(e.target).length === 0) {
+      $('.modal--cart').removeClass('modal--active');
+      $('body').removeClass('scroll-lock');
+    };
+  });
+
+  $('body').on('click', '.quantity__btn--minus, .quantity__btn--plus', function () {
+    var $row = $(this).closest('.quantity');
+    var $input = $row.find('.quantity__text');
+    var step = $row.data('step');
+    var val = parseFloat($input.val());
+    if ($(this).hasClass('quantity__btn--minus')) {
+      val -= step;
+    } else {
+      val += step;
+    }
+    $input.val(val);
+    $input.change();
+    return false;
+  });
+
+  $('body').on('change', '.quantity__text', function () {
+    var $input = $(this);
+    var $row = $input.closest('.quantity');
+    var step = $row.data('step');
+    var min = parseInt($row.data('min'));
+    var max = parseInt($row.data('max'));
+    var val = parseFloat($input.val());
+    if (isNaN(val)) {
+      val = step;
+    } else if (min && val < min) {
+      val = min;
+    } else if (max && val > max) {
+      val = max;
+    }
+    $input.val(val);
+  });
+
+
+
+  // Slick слайдер
+  $('.discounts__list').slick({
+
+    autoplay: false,
+    arrows: true,
+    infinite: false,
+    swipeToSlide: true,
+    dots: false,
+    prevArrow: '<button class="slick-btn slick-btn--prev" type="button"><svg class="icon icon--user"><use xlink: href="images/icons/icons.svg#arrow-left"></use></svg ><span class="sr-only">Переключить на предыдущий слайд</span></button>',
+    nextArrow: '<button class="slick-btn slick-btn--next" type="button"><svg class="icon icon--user"><use xlink: href="images/icons/icons.svg#arrow-right"></use></svg ><span class="sr-only">Переключить на следующий слайд</span></button>',
+
+        // the magic
+    responsive: [{
+
+      breakpoint: 1500,
+      settings: {
+        swipeToSlide: true,
+        infinite: true,
+        arrows: false,
+        dots: true,
+        infinite: true
+      }
+
+    }]
+
+/*
+    // normal options...
+    infinite: false,
+
+    // the magic
+    responsive: [{
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+      }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+
+    }, {
+
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+
+    }]
+*/
+  });
+
+  var mixer = mixitup('.bestsellers__list');
+});
